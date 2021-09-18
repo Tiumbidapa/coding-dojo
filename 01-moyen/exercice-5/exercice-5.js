f96ef771185a18eefbe616498cab5e5cefab138e
Laurine <21705826@etu.unicaen.fr>
2021-09-18 13:55:28 +0200

    Exercice 5 du niveau moyen complété.

--- 01-moyen/exercice-5/exercice-5.js
+++ 01-moyen/exercice-5/exercice-5.js
@@ -1,6 +1,12 @@
 function exercice5(tableau) {
-
+  somme = 0;
+  for (let elem of tableau){
+    somme += elem;
+  }
+  return somme;
 }
 
+const tab1 = console.log(exercice5([1, 2, 3, 4]));
+const tab2 = console.log(exercice5([1, 2, 3, 4, 10, 11]));
 // Ne touchez pas à la ligne suivante
 module.exports = exercice5;
